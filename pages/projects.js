626965ecfc200195f6bfbaf06953fa4c864cdbbd
vonsteer <jconstante@redpoints.com>
2021-04-02 19:01:40 +0200

    removed cv.js and remaned some css modules

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/projects.module.css";
+import styles from "../styles/Projects.module.css";
 import { Navbar } from "../components/navbar";
 
 const Projects = ({ projects }) => {
@@ -16,17 +16,5 @@ const Projects = ({ projects }) => {
   );
 };
 
-export const getServerSideProps = async (pageContext) => {
-  const apiResponse = await fetch(
-    "https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth"
-  );
-  const employee = await apiResponse.json();
-
-  return {
-    props: {
-      employee,
-    },
-  };
-};
 
 export default Projects;
