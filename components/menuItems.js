530fa5e581854c9fb8bb2794ef3ba731fa46bd6b
vonsteer <jconstante@redpoints.com>
2021-04-26 00:21:53 +0200

    added nav hamburger for mobile

---
+++ components/menuItems.js
@@ -0,0 +1,24 @@
+const MenuItems = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: null,
+  },
+  {
+    title: 'Blog',
+    path: '/blog/1',
+    cName: null,
+  },
+  {
+    title: 'Projects',
+    path: '/projects',
+    cName: null,
+  },
+  {
+    title: 'Resume',
+    path: '/resume.pdf',
+    cName: 'button',
+  }
+];
+
+export default MenuItems;
